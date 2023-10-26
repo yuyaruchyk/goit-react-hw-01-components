cd533725d10999976760885809acd61e65615ce1
yuyaruchyk <130397548+yuyaruchyk@users.noreply.github.com>
2023-10-26 10:28:34 +0200

    upd
    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import user from "components/Profile/user.json";
 import { Profile } from "./Profile/Profile"
+import data from "components/Statistics/data.json";
+import { Statistics } from "./Statistics/Statistics"
+
 
 
 export const App = () => {
@@ -13,6 +16,11 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+    <Statistics title="Upload stats" stats={data} />
+
+
+
 
     </div>
   );
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import data from "components/Statistics/data.json";
+
+export function Statistics() {
+  return (
+    <section className="statistics">
+      <h2 className="title">Upload stats</h2>
+
+      <ul className="stat-list">
+        {data.map(({ id, label, percentage }) => (
+          <li className="item" key={id}>
+            <span className="label">{label}</span>
+            <span className="percentage">{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+
+
+
+
+
